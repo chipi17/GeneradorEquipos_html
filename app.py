b9675e0ef21bdaca67383ef680b239d6e3214797
chipi17 <129787986+chipi17@users.noreply.github.com>
2023-04-03 18:59:27 +0000

    Pending changes exported from your codespace

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import random
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        nombres = request.form.getlist('nombre')
+        random.shuffle(nombres)
+        mitad = len(nombres) // 2
+        equipo_A = nombres[:mitad]
+        equipo_B = nombres[mitad:]
+        return render_template('equipos.html', equipo_A=equipo_A, equipo_B=equipo_B)
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
